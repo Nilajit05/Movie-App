91f39565c11b9176d9f1c01ab758b7a16ef4722a
Nilajit <nilajitgope51@gmail.com>
2023-11-23 00:10:52 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import MovieList from "./Movielist" ; 
+
+
+function App() {
+  return (
+   <>
+   <h1 > Movie App</h1>
+   <MovieList/>
+   </>
+  );
+}
+
+export default App;
